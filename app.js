ce0d15d6b1a2679ca7851d98209dc3a0a3ddab3f
samcjswo <samcjswok@gmail.com>
2025-12-21 21:54:09 +0900

    251221_js4.2

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
 
-function handleTitleClick(){
-    console.log("Title was clicked!");
-    h1.style.color = "blue";
+function onLoginSubmit(event) {
+    const username = loginInput.value;
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-    console.log("Mouse is here!");
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-    console.log("Mouse is gone!");
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
+loginForm.addEventListener("submit", onLoginSubmit);
