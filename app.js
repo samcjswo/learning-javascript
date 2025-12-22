5f58b70b8ea284a9c9ed4fd278e400fd2541b7b4
samcjswo <samcjswok@gmail.com>
2025-12-22 21:29:06 +0900

    251222_js4.5

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
-    const username = loginInput.value;
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
